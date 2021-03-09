b48f74cfa7e1fc33c15f4ba247716103645809c3
LucasFlaquer <lucas.flaquer@gmail.com>
2021-03-08 23:13:54 -0300

    inport css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import './styles/global.css'
+
 export function App() {
-  throw new Error('Deu ruim aqui no codigoc')
+  // throw new Error('Deu ruim aqui no codigoc')
 
   return <h1>Hello World</h1>
 }
