a0a546b23136515fc73eb7c042231f53c7321a19
Lucas Flaquer <lucas.flaquer@crbconstrutora.com.br>
2021-03-09 17:56:42 -0300

    webpack e components

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,38 +1,23 @@
-export function RepositoryList() {
-    return (
-        <section className="repository-list">
-            <h1>Lista de Repositórios</h1>
+import { RepositoryItem } from "./RepositoryItem";
 
-            <ul>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="#">
-                        Acessar Repositório
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="#">
-                        Acessar Repositório
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="#">
-                        Acessar Repositório
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="#">
-                        Acessar Repositório
-                    </a>
-                </li>
-            </ul>
-        </section>
-    )
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: "https:github.com/unform/unform"
+}
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+      <ul>
+        
+      <RepositoryItem repository={repository}/>
+      <RepositoryItem repository={repository}/>
+      <RepositoryItem repository={repository}/>
+      
+      
+          
+      </ul>
+    </section>
+  )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -16,20 +17,30 @@ module.exports = {
     extensions: ['.js', '.jsx']
   },
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
-      
+      inject: 'body'
     })
-  ],
+  ].filter(Boolean),
   devServer: {
-    contentBase: path.resolve(__dirname, 'public')
+    contentBase: path.resolve(__dirname, 'public'),
+    hot: true
   },
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        },
+
       },
       {
         test: /\.scss/,
