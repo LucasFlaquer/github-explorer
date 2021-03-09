6145275a71f0acd8c8f8fb409b2bc54aeef6a581
LucasFlaquer <lucas.flaquer@gmail.com>
2021-03-09 19:38:15 -0300

    remove error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './styles/global.scss'
 
 export function App() {
-  // throw new Error('Deu ruim aqui no codigoc')
+  throw new Error('Deu ruim aqui no codigo')
 
   return <h1>Hello World</h1>
 }
