ece8f329e66cbc0c5eaffb10d8c9c5270698cc16
LucasFlaquer <lucas.flaquer@gmail.com>
2021-03-09 22:28:52 -0300

    listagem de repositorios com map

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,24 @@
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: "https:github.com/unform/unform"
-}
+import "../styles/repositories.scss"
+import { useEffect, useState } from "react";
+
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+  useEffect(() => {
+    fetch('https://api.github.com/users/LucasFlaquer/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
+      <p>Listagem de todos os {repositories.length}</p>
       <ul>
-        
-      <RepositoryItem repository={repository}/>
-      <RepositoryItem repository={repository}/>
-      <RepositoryItem repository={repository}/>
-      
-      
-          
+        {repositories.map(repository => {
+          return <RepositoryItem repository={repository} key={repository.name} />
+        })}
       </ul>
     </section>
   )
